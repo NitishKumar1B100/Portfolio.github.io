a40112042672b2a3706cfdd31f3f539ff7b45f9e
Nitish Kumar <140237257+NitishKumar1B100@users.noreply.github.com>
2024-02-05 21:49:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function openMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+}
+
+openLink = document.getElementById('openLink').addEventListener('click',()=>{
+    window.open('https://github.com/NitishKumar1B100','_blank')
+})
+
+connectLink = document.getElementById('connectLink').addEventListener('click',()=>{
+    window.open('https://www.linkedin.com/in/nitish-k-a2877a250/','_blank')
+})
+
+// https://drive.google.com/file/d/1j94nCzfHq7h2-30uFpNghsiFREUAM-y8/view?usp=sharing
+
+let resumeLink1 = document.getElementById('resumeLink1').addEventListener('click',()=>{
+    console.log("clik");
+    window.open('https://drive.google.com/file/d/1j94nCzfHq7h2-30uFpNghsiFREUAM-y8/view?usp=sharing','_blank')
+})
+
+let resumeLink2 = document.getElementById('resumeLink2').addEventListener('click',()=>{
+    console.log("clik");
+    window.open('https://drive.google.com/file/d/1j94nCzfHq7h2-30uFpNghsiFREUAM-y8/view?usp=sharing','_blank')
+})
+
+
+let openMailTo = document.getElementById('openMailTo').addEventListener('click',()=>{
+    let mail = 'nitishkumar235969@gmail.com'
+    window.location.href='mailto:'+mail
+})
+
+let openContactTo = document.getElementById('openContactTo').addEventListener('click',()=>{
+
+})
+
+let openLinkedinTo = document.getElementById('openLinkedinTo').addEventListener('click',()=>{
+    window.open('https://www.linkedin.com/in/nitish-k-a2877a250/','_blank')
+})
+
+let openGithubTo = document.getElementById('openGithubTo').addEventListener('click',()=>{
+    window.open('https://github.com/NitishKumar1B100','_blank')
+})
